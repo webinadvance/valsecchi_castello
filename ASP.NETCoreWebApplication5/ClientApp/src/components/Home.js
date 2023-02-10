0759c61675db94c39c005c6e338f10dc4ae61ee4
MatteoMaggiolini <webinadvance@gmail.com>
2023-02-10 14:07:53 +0100

    sdffsf

--- ASP.NETCoreWebApplication5/ClientApp/src/components/Home.js
+++ ASP.NETCoreWebApplication5/ClientApp/src/components/Home.js
@@ -4,12 +4,12 @@ export function Home() {
     return (
         <Fragment>
             <div id={"headerarea"} className={"relative"}>
+                <video id={"videoblog"} autoPlay loop muted playsInline preload="metadata" id="videobg">
+                    <source
+                        src="https://s3.amazonaws.com/uploads.serenohotels.com/app/uploads/2016/08/09220547/Villa-Pliniana-Descriptivo-v3_low.mp4"
+                        type="video/mp4"/>
+                </video>
             </div>
-            <video autoPlay loop muted playsInline preload="metadata" id="videobg">
-                <source
-                    src="https://s3.amazonaws.com/uploads.serenohotels.com/app/uploads/2016/08/09220547/Villa-Pliniana-Descriptivo-v3_low.mp4"
-                    type="video/mp4"/>
-            </video>
         </Fragment>
     );
 }
