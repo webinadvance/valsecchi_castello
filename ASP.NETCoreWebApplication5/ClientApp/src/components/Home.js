e02e8fb04bc31725161b56ea0b81809d6318e60b
MatteoMaggiolini <83645979+MatteoMaggiolini@users.noreply.github.com>
2023-02-13 15:38:46 +0100

    dsasaas

--- ASP.NETCoreWebApplication5/ClientApp/src/components/Home.js
+++ ASP.NETCoreWebApplication5/ClientApp/src/components/Home.js
@@ -1,8 +1,17 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, useState} from 'react';
+import Navbar from "./Navbar";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faBars, faCoffee} from "@fortawesome/free-solid-svg-icons";
 
 export function Home() {
+
+    const [isOpen, setIsOpen] = useState(false);
+
     return (<Fragment>
         <div id={"headerarea"} className={"relative"}>
+            <div className={"absolute z-10 uppercase w-full p-10"}>
+                <Navbar/>
+            </div>
             <video id={"videoblog"} autoPlay loop muted playsInline preload="metadata" id="videobg">
                 <source
                     src="https://s3.amazonaws.com/uploads.serenohotels.com/app/uploads/2016/08/09220547/Villa-Pliniana-Descriptivo-v3_low.mp4"
@@ -14,5 +23,6 @@ export function Home() {
                 "THE MOST LUXURIOUS VILLA ON LAKE COMO"
             </div>
         </div>
+
     </Fragment>);
 }
---
+++ ASP.NETCoreWebApplication5/ClientApp/src/components/Navbar.js
@@ -0,0 +1,35 @@
+﻿import React, {Fragment, useState} from "react";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faBars} from "@fortawesome/free-solid-svg-icons";
+
+const Navbar = () => {
+
+    const [isOpen, setIsOpen] = useState(false);
+    const elements = ["VILLA", "ACCOMMODATION", "FACILITIES", "WEDDINGS & EVENTSHISTOR", "YGALLERY", "PRESS", "CONTACTS"];
+
+    return (
+        <Fragment>
+            <nav className="flex items-center justify-between p-4 text-white">
+                <div className="flex items-center">
+                    <a href="#" className="font-bold text-xl">
+                        My App
+                    </a>
+                </div>
+                <div className="flex items-center sm:hidden">
+                    <FontAwesomeIcon icon={faBars} size={"2x"}/>
+                </div>
+                <div className="hidden sm:flex sm:items-center sm:w-auto">
+                    {elements.map((x, i) => {
+                        return (
+                            <a href="#" className="px-4 text-white text-sm font-light">
+                                {x}
+                            </a>
+                        )
+                    })}
+                </div>
+            </nav>
+        </Fragment>
+    );
+};
+
+export default Navbar;
