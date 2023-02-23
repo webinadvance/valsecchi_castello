9c8326fa1baa907375b49f568a8292f6cf982468
webinadvance <webinadvance@gmail.com>
2023-02-23 13:43:21 +0100

    ewqewq

--- ASP.NETCoreWebApplication5/ClientApp/src/components/LanguageSwitcher.tsx
+++ ASP.NETCoreWebApplication5/ClientApp/src/components/LanguageSwitcher.tsx
@@ -3,55 +3,49 @@ import {IoLanguage} from 'react-icons/io5';
 import {IconType} from 'react-icons/lib';
 import {useTranslation} from "react-i18next";
 
-type Language = 'en' | 'it';
-
-const languageNames: Record<Language, string> = {
+const languageNames: Record<string, string> = {
     en: 'English',
     it: 'Italiano',
 };
 
-const languageIcons: Record<Language, string> = {
+const languageIcons: Record<string, string> = {
     en: 'https://hatscripts.github.io/circle-flags/flags/us.svg',
     it: 'https://hatscripts.github.io/circle-flags/flags/it.svg',
 };
 
 function LanguageSwitcher() {
-    const [language, setLanguage] = useState<Language>('en');
 
-    const handleLanguageChange = (newLanguage: Language) => {
+    const handleLanguageChange = (newLanguage: string) => {
         i18n.changeLanguage(newLanguage);
-        setLanguage(newLanguage);
     };
 
     const {i18n} = useTranslation();
-    
+
     return (
         <div className="flex flex-column items-center">
             {Object.keys(languageNames).map((lang) => {
-                const languageCode = lang as Language;
+                const languageCode = lang as string;
                 return (
-                    <div>
-                        <button
-                            key={languageCode}
-                            className={`mr-2 ${
-                                languageCode === language ? 'text-blue-500' : ''
-                            }`}
-                            onClick={() => handleLanguageChange(languageCode)}
-                        >
-                            <div className="flex items-center">
-                                <img
-                                    src={languageIcons[languageCode]}
-                                    alt={`${languageNames[languageCode]} flag`}
-                                    className="w-4 h-4 mr-1 rounded-full"
-                                />
-                                {languageNames[languageCode]}
-                            </div>
-                        </button>
-                    </div>
+                    <button
+                        key={languageCode}
+                        className={`mr-2 ${
+                            languageCode === i18n.language ? 'text-blue-500' : ''
+                        }`}
+                        onClick={() => handleLanguageChange(languageCode)}
+                    >
+                        <div className="flex items-center">
+                            <img
+                                src={languageIcons[languageCode]}
+                                alt={`${languageNames[languageCode]} flag`}
+                                className="w-4 h-4 mr-1 rounded-full"
+                            />
+                            {languageNames[languageCode]}
+                        </div>
+                    </button>
                 );
             })}
         </div>
     );
 }
 
-export default LanguageSwitcher;
+    export default LanguageSwitcher;
