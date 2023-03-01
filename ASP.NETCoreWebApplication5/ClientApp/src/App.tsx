bb8c3c12f8b1a119d26f13ba8c48b78e7e6c52a1
MatteoMaggiolini <webinadvance@gmail.com>
2023-03-01 20:55:35 +0100

    dsadsa

--- ASP.NETCoreWebApplication5/ClientApp/src/App.tsx
+++ ASP.NETCoreWebApplication5/ClientApp/src/App.tsx
@@ -1,39 +1,49 @@
 import {useEffect, useState} from 'react';
-import {Route, Routes, useMatch} from 'react-router-dom';
+import {useDispatch} from 'react-redux';
 import {useCookies} from 'react-cookie';
-import {useDispatch, useSelector} from 'react-redux';
 import {makeStyles} from '@mui/styles';
 import {Fab, Theme, Zoom} from '@mui/material';
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
-
+import {Route, Routes, useMatch} from 'react-router-dom';
+import useAsyncEffect from 'use-async-effect';
 import i18n from 'i18next';
 
 import './custom.css';
 
-import {routes} from './Globals';
 import Api from './Api';
-import {RootState} from './Store';
+import {routes} from './Globals';
 import {user} from './dataSlice';
 
+import Admin from './components/Admin';
 import Footer from './components/Footer';
 import Header from './components/Header';
-import Admin from './components/Admin';
 import Home from './components/Home';
 import Loader from './components/Loader';
 
-export default function App() {
-    const [cookies, setCookie] = useCookies(['preferredLanguage']);
-    const [preferredLanguage, setPreferredLanguage] = useState(
-        cookies.preferredLanguage || 'en'
-    );
+const App = () => {
+
+    const [cookie, setCookie] = useCookies(['preferredLanguage']);
+    const [language, setLanguage] = useState(cookie.preferredLanguage || 'en');
+    const [showScrollButton, setShowScrollButton] = useState(false);
+
+    const dispatch = useDispatch();
+    const isAdminPage = useMatch('/admin');
+
+    const scrollToTop = () => {
+        window.scrollTo({top: 0, behavior: 'smooth'});
+    };
+
+    const handleScroll = () => {
+        setShowScrollButton(window.scrollY > 1);
+    };
 
     const useStyles = makeStyles((theme: Theme) => ({
-        root: {
+        scrollButton: {
             position: 'fixed',
             bottom: theme.spacing(2),
             right: theme.spacing(2),
         },
-        button: {
+        fabButton: {
             backgroundColor: theme.palette.primary.main,
             color: theme.palette.primary.contrastText,
             '&:hover': {
@@ -41,51 +51,38 @@ export default function App() {
             },
         },
     }));
+
     const classes = useStyles();
 
-    const [showButton, setShowButton] = useState(false);
-    const handleClick = () => {
-        window.scrollTo({top: 0, behavior: 'smooth'});
-    };
-    const handleScroll = () => {
-        setShowButton(window.scrollY > 1);
-    };
-
     useEffect(() => {
-        setCookie('preferredLanguage', preferredLanguage, {path: '/'});
-        i18n.changeLanguage(preferredLanguage);
-    }, [preferredLanguage, setCookie]);
+        setCookie('preferredLanguage', language, {path: '/'});
+        i18n.changeLanguage(language);
+    }, [language, setCookie]);
 
-    const dispatch = useDispatch();
-    const userState = useSelector((state: RootState) => state.data.user);
-    useEffect(() => {
-        const getUser = async () => {
-            const res = await Api.user();
-            dispatch(user(res));
-        };
-        getUser();
+    useAsyncEffect(async () => {
+        const response = await Api.user();
+        dispatch(user(response));
         window.addEventListener('scroll', handleScroll);
         return () => window.removeEventListener('scroll', handleScroll);
     }, [dispatch]);
 
-    const isMatch = useMatch('/admin');
-    const header = !isMatch && <Header/>;
-    const footer = !isMatch && <Footer/>;
+    const header = !isAdminPage && <Header/>;
+    const footer = !isAdminPage && <Footer/>;
 
     return (
         <>
             {header}
             <Routes>
                 <Route path="/" element={<Home/>}/>
-                {routes.map(({key, element}, i) => (
-                    <Route key={i} path={key} element={element}/>
+                {routes.map(({key, element}, index) => (
+                    <Route key={index} path={key} element={element}/>
                 ))}
                 <Route path="/admin" element={<Admin/>}/>
             </Routes>
             {footer}
-            <Zoom in={showButton}>
-                <div onClick={handleClick} role="presentation" className={classes.root}>
-                    <Fab className={classes.button} size="small">
+            <Zoom in={showScrollButton}>
+                <div onClick={scrollToTop} role="presentation" className={classes.scrollButton}>
+                    <Fab className={classes.fabButton} size="small">
                         <KeyboardArrowUpIcon/>
                     </Fab>
                 </div>
@@ -93,6 +90,6 @@ export default function App() {
             <Loader/>
         </>
     );
-}
 
-App.displayName = App.name;
+}
+export default App;
