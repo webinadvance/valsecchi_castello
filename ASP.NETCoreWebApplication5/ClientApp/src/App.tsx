ac68c25cd2518bf43ab977c79e6e344089bc53bb
MatteoMaggiolini <webinadvance@gmail.com>
2023-03-03 20:56:39 +0100

    fdsf

--- ASP.NETCoreWebApplication5/ClientApp/src/App.tsx
+++ ASP.NETCoreWebApplication5/ClientApp/src/App.tsx
@@ -1,7 +1,7 @@
 import React, {FC, lazy, memo, Suspense, useCallback, useEffect, useMemo, useState} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
 import {useCookies} from 'react-cookie';
-import {createStyles, makeStyles} from '@mui/styles';
+import {makeStyles} from '@mui/styles';
 import {Fab, Theme, Zoom} from '@mui/material';
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 import {Route, Routes, useMatch} from 'react-router-dom';
@@ -20,22 +20,20 @@ const Footer = lazy(() => import('./components/Footer'));
 const Header = lazy(() => import('./components/Header'));
 const Loader = lazy(() => import('./components/Loader'));
 
-const useStyles = makeStyles((theme: Theme) =>
-    createStyles({
-        scrollButton: {
-            position: 'fixed',
-            bottom: theme.spacing(2),
-            right: theme.spacing(2),
+const useStyles = makeStyles((theme: Theme) => ({
+    scrollButton: {
+        position: 'fixed',
+        bottom: theme.spacing(2),
+        right: theme.spacing(2),
+    },
+    fabButton: {
+        backgroundColor: theme.palette.primary.main,
+        color: theme.palette.primary.contrastText,
+        '&:hover': {
+            backgroundColor: theme.palette.primary.dark,
         },
-        fabButton: {
-            backgroundColor: theme.palette.primary.main,
-            color: theme.palette.primary.contrastText,
-            '&:hover': {
-                backgroundColor: theme.palette.primary.dark,
-            },
-        },
-    })
-);
+    },
+}));
 
 interface IProps {
 }
