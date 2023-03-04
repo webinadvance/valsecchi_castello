52f9516ef338b1218d3e221a4a38b651e6fc27b3
MatteoMaggiolini <webinadvance@gmail.com>
2023-03-04 18:58:37 +0100

    fdf

--- ASP.NETCoreWebApplication5/ClientApp/src/components/AdminLang.tsx
+++ ASP.NETCoreWebApplication5/ClientApp/src/components/AdminLang.tsx
@@ -1,9 +1,9 @@
 import React, {useEffect} from "react";
-import 'react-quill/dist/quill.snow.css';
 import Api from "../Api";
-import {AiTable} from "./AiEdit";
-import {AppBar, Button, Theme, Toolbar, Typography} from "@mui/material";
+import {AppBar, Button, Menu, MenuItem, Theme, Toolbar, Typography, useMediaQuery,} from "@mui/material";
 import {createStyles, makeStyles} from "@mui/styles";
+import clsx from "clsx";
+import {AiTable} from "./AiEdit";
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
@@ -13,6 +13,15 @@ const useStyles = makeStyles((theme: Theme) =>
         toolbar: {
             display: "flex",
             justifyContent: "space-between",
+            [theme.breakpoints.down("sm")]: {
+                flexDirection: "column",
+                alignItems: "center",
+            },
+        },
+        toolbarSmall: {
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
         },
     })
 );
@@ -43,17 +52,61 @@ const AdminLang = React.memo(function () {
     }, []);
 
     const [data2, setData2] = React.useState(initialData);
+    const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
 
     const classes = useStyles();
+    const isSmallScreen = useMediaQuery((theme: Theme) =>
+        theme.breakpoints.down("sm")
+    );
+
+    const handleMenuOpen = (event: React.MouseEvent<HTMLButtonElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleMenuClose = () => {
+        setAnchorEl(null);
+    };
 
     return (
         <>
             <AppBar position="fixed" className={classes.appBar}>
-                <Toolbar className={classes.toolbar}>
+                <Toolbar
+                    className={clsx(classes.toolbar, {
+                        [classes.toolbarSmall]: isSmallScreen,
+                    })}
+                >
                     <Typography variant="h6" noWrap>
                         My App
                     </Typography>
-                    <Button color="inherit">Login</Button>
+                    {isSmallScreen ? (
+                        <>
+                            <Button color="inherit" onClick={handleMenuOpen}>
+                                Menu
+                            </Button>
+                            <Menu
+                                anchorEl={anchorEl}
+                                keepMounted
+                                open={Boolean(anchorEl)}
+                                onClose={handleMenuClose}
+                            >
+                                <MenuItem onClick={handleMenuClose}>Login</MenuItem>
+                                <MenuItem onClick={handleMenuClose}>Login1</MenuItem>
+                                <MenuItem onClick={handleMenuClose}>Login2</MenuItem>
+                                <MenuItem onClick={handleMenuClose}>Login3</MenuItem>
+                                <MenuItem onClick={handleMenuClose}>Login4</MenuItem>
+                                <MenuItem onClick={handleMenuClose}>Login5</MenuItem>
+                            </Menu>
+                        </>
+                    ) : (
+                        <>
+                            <Button color="inherit">Login</Button>
+                            <Button color="inherit">Login1</Button>
+                            <Button color="inherit">Login2</Button>
+                            <Button color="inherit">Login3</Button>
+                            <Button color="inherit">Login4</Button>
+                            <Button color="inherit">Login5</Button>
+                        </>
+                    )}
                 </Toolbar>
             </AppBar>
             <Toolbar/>
@@ -62,15 +115,15 @@ const AdminLang = React.memo(function () {
                 onNew={() => {
                     return {key: ""} as Lang;
                 }}
-                onDelete={async (row) => {
+                onDelete={async (row: any) => {
                     await Api.deleteadminlang(row as Lang);
                     await loadData();
                 }}
-                columns={[
-                    {key: "key", label: "key", colspan: 1, readonly: false},
-                    {key: "en", label: "en", colspan: 2},
-                    {key: "it", label: "it", colspan: 2},
-                ]}
+                columns={[{key: "key", label: "key", colspan: 1, readonly: false}, {
+                    key: "en",
+                    label: "en",
+                    colspan: 2
+                }, {key: "it", label: "it", colspan: 2},]}
                 onSave={async (obj: Lang) => {
                     setData2((prevData) =>
                         prevData.map((p) => (p.key === obj.key ? obj : p))
