ed0c46fc6844c85532e3796ae76d3a36eb368db7
MatteoMaggiolini <webinadvance@gmail.com>
2023-03-06 18:26:37 +0100

    dsadsad

--- ASP.NETCoreWebApplication5/ClientApp/src/components/Home.tsx
+++ ASP.NETCoreWebApplication5/ClientApp/src/components/Home.tsx
@@ -1,25 +1,18 @@
-import {Fragment} from 'react';
-import {useTranslation} from 'react-i18next';
-import Services from './Services';
 import {useInView} from 'react-intersection-observer';
 import {animated, useSpring} from 'react-spring';
+import {Fragment, ReactNode} from 'react';
+import {useTranslation} from 'react-i18next';
+import Services from './Services';
 
-const Home = () => {
-    const {t} = useTranslation();
+interface AnimatedSectionProps {
+    children: ReactNode;
+}
+
+const AnimatedSection = ({children}: AnimatedSectionProps) => {
     const [ref, inView] = useInView({
         threshold: 0.5,
         triggerOnce: true,
     });
-
-    const [refServices, inViewServices] = useInView({
-        threshold: 0.5,
-        triggerOnce: true,
-
-    });
-
-    const welcomeMessages = [...Array(100)].filter(
-        (_, i) => t(`welcome${i + 1}`) !== `welcome${i + 1}`
-    );
 
     const slideIn = useSpring({
         opacity: inView ? 1 : 0,
@@ -30,37 +23,43 @@ const Home = () => {
         },
     });
 
-    const slideInServices = useSpring({
-        opacity: inViewServices ? 1 : 0,
-        transform: inViewServices ? 'translateX(0)' : 'translateX(-100px)',
-        config: {
-            duration: 800,
-            easing: t => -0.5 * (Math.cos(Math.PI * t) - 1),
-        },
-    });
+    return (
+        <animated.section ref={ref} style={slideIn}>
+            {children}
+        </animated.section>
+    );
+};
+
+const Home = () => {
+    const {t} = useTranslation();
+
+    const welcomeMessages = [...Array(100)].filter(
+        (_, i) => t(`welcome${i + 1}`) !== `welcome${i + 1}`,
+    );
 
     return (
-        <>
-            <div className="text-center m-auto">
-                <animated.section ref={ref} style={slideIn}>
-                    <div id="welcome" className="ch1 uppercase mt-6 text-center m-auto pt-8">
-                        «{t('home1')}»
-                    </div>
-                    <div className="divider"/>
-                    <article>
-                        {welcomeMessages.map((_, i, arr) => (
-                            <Fragment key={i}>
-                                <div>{t(`welcome${i + 1}`)}</div>
-                                {i !== arr.length - 1 && <div className="divider"/>}
-                            </Fragment>
-                        ))}
-                    </article>
-                </animated.section>
-                <animated.section ref={refServices} style={slideInServices}>
-                    <Services/>
-                </animated.section>
-            </div>
-        </>
+        <div className="text-center m-auto">
+            <AnimatedSection>
+                <div
+                    id="welcome"
+                    className="ch1 uppercase mt-6 text-center m-auto pt-8"
+                >
+                    «{t('home1')}»
+                </div>
+                <div className="divider"/>
+                <article>
+                    {welcomeMessages.map((_, i, arr) => (
+                        <Fragment key={i}>
+                            <div>{t(`welcome${i + 1}`)}</div>
+                            {i !== arr.length - 1 && <div className="divider"/>}
+                        </Fragment>
+                    ))}
+                </article>
+            </AnimatedSection>
+            <AnimatedSection>
+                <Services/>
+            </AnimatedSection>
+        </div>
     );
 };
 
