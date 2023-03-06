5abed09aa15da65a577e7c65ba18f2023294cafe
MatteoMaggiolini <webinadvance@gmail.com>
2023-03-06 18:27:35 +0100

    dsadsad

---
+++ ASP.NETCoreWebApplication5/ClientApp/src/components/AnimatedSection.tsx
@@ -0,0 +1,29 @@
+﻿import {useInView} from "react-intersection-observer";
+import {animated, useSpring} from "react-spring";
+import {ReactNode} from "react";
+
+export interface AnimatedSectionProps {
+    children: ReactNode;
+}
+
+export const AnimatedSection = ({children}: AnimatedSectionProps) => {
+    const [ref, inView] = useInView({
+        threshold: 0.5,
+        triggerOnce: true,
+    });
+
+    const slideIn = useSpring({
+        opacity: inView ? 1 : 0,
+        transform: inView ? 'translateX(0)' : 'translateX(-100px)',
+        config: {
+            duration: 800,
+            easing: t => -0.5 * (Math.cos(Math.PI * t) - 1),
+        },
+    });
+
+    return (
+        <animated.section ref={ref} style={slideIn}>
+            {children}
+        </animated.section>
+    );
+};
--- ASP.NETCoreWebApplication5/ClientApp/src/components/Home.tsx
+++ ASP.NETCoreWebApplication5/ClientApp/src/components/Home.tsx
@@ -1,34 +1,7 @@
-import {useInView} from 'react-intersection-observer';
-import {animated, useSpring} from 'react-spring';
-import {Fragment, ReactNode} from 'react';
+import {Fragment} from 'react';
 import {useTranslation} from 'react-i18next';
 import Services from './Services';
-
-interface AnimatedSectionProps {
-    children: ReactNode;
-}
-
-const AnimatedSection = ({children}: AnimatedSectionProps) => {
-    const [ref, inView] = useInView({
-        threshold: 0.5,
-        triggerOnce: true,
-    });
-
-    const slideIn = useSpring({
-        opacity: inView ? 1 : 0,
-        transform: inView ? 'translateX(0)' : 'translateX(-100px)',
-        config: {
-            duration: 800,
-            easing: t => -0.5 * (Math.cos(Math.PI * t) - 1),
-        },
-    });
-
-    return (
-        <animated.section ref={ref} style={slideIn}>
-            {children}
-        </animated.section>
-    );
-};
+import {AnimatedSection} from "./AnimatedSection";
 
 const Home = () => {
     const {t} = useTranslation();
