ef45f13aa6fca6bcd40afe8b4eb50e170853c859
MatteoMaggiolini <webinadvance@gmail.com>
2023-03-10 18:57:32 +0100

    dsad

--- ASP.NETCoreWebApplication5/ClientApp/src/components/Gallery1.tsx
+++ ASP.NETCoreWebApplication5/ClientApp/src/components/Gallery1.tsx
@@ -1,10 +1,12 @@
 import React, {Fragment, useEffect, useState} from "react";
 import PhotoGallery from "./PhotoGallery";
 import useMediaQuery from "@mui/material/useMediaQuery";
+import {useTranslation} from "react-i18next";
 
 const Gallery1: React.FC = () => {
     const [data, setData] = useState<any>([]);
     const isMobile = useMediaQuery('(max-width:1024px)');
+    const {t} = useTranslation()
 
     useEffect(() => {
         (async () => {
@@ -18,7 +20,7 @@ const Gallery1: React.FC = () => {
         <div className={"mb-4"} id={"welcome"}>
             {data && data.map((_: any, i: any) => (
                 <Fragment key={i}>
-                    <h2>{_.title}</h2>
+                    <h2>{t(_.title)}</h2>
                     <PhotoGallery images={_.data}/>
                 </Fragment>
             ))}
