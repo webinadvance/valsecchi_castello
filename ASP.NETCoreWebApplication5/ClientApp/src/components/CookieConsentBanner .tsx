2ce7ea1b6d91154967ff3558b86283b85fed1005
webinadvance <webinadvance@gmail.com>
2023-03-11 14:09:05 +0100

    fdfds

--- ASP.NETCoreWebApplication5/ClientApp/src/components/CookieConsentBanner .tsx
+++ ASP.NETCoreWebApplication5/ClientApp/src/components/CookieConsentBanner .tsx
@@ -1,6 +1,9 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {makeStyles} from '@mui/styles';
 import {Drawer, Box, Typography, Button, Theme} from '@mui/material';
+
+const COOKIE_CONSENT_KEY = 'cookie_consent';
+const ALLOWED_COOKIES = ['functional', 'analytics', 'marketing'];
 
 const useStyles = makeStyles((theme: Theme) => ({
     drawer: {
@@ -26,12 +29,28 @@ const CookieConsentBanner = () => {
     const [allowAnalyticsCookies, setAllowAnalyticsCookies] = useState(false);
     const [allowMarketingCookies, setAllowMarketingCookies] = useState(false);
 
+    useEffect(() => {
+        const cookieConsent = localStorage.getItem(COOKIE_CONSENT_KEY);
+        if (cookieConsent) {
+            const parsedConsent = JSON.parse(cookieConsent);
+            setAllowFunctionalCookies(parsedConsent.functional);
+            setAllowAnalyticsCookies(parsedConsent.analytics);
+            setAllowMarketingCookies(parsedConsent.marketing);
+            setIsOpen(false);
+        }
+    }, []);
+
     const handleClose = () => {
         setIsOpen(false);
     };
 
     const handleSave = () => {
-        // Save the user's cookie consent preferences here
+        const cookieConsent = {
+            functional: allowFunctionalCookies,
+            analytics: allowAnalyticsCookies,
+            marketing: allowMarketingCookies,
+        };
+        localStorage.setItem(COOKIE_CONSENT_KEY, JSON.stringify(cookieConsent));
         setIsOpen(false);
     };
 
@@ -46,6 +65,15 @@ const CookieConsentBanner = () => {
     const handleToggleMarketingCookies = () => {
         setAllowMarketingCookies(!allowMarketingCookies);
     };
+
+    useEffect(() => {
+        const allowed = ALLOWED_COOKIES.every(
+            (cookieType) => (cookieType === 'functional' && allowFunctionalCookies) ||
+                (cookieType === 'analytics' && allowAnalyticsCookies) ||
+                (cookieType === 'marketing' && allowMarketingCookies),
+        );
+        setIsOpen(!allowed);
+    }, [allowFunctionalCookies, allowAnalyticsCookies, allowMarketingCookies]);
 
     return (
         <Drawer
@@ -65,23 +93,32 @@ const CookieConsentBanner = () => {
                 <Typography variant="body1" gutterBottom>
                     We use cookies to personalise content and ads, to provide social media features and to analyse our
                     traffic. We also share information about your use of our site with our social media, advertising and
-                    analytics partners who may combine it with other information that you’ve provided to them or that
-                    they’ve collected from your use of their services.
+                    analytics partners who may combine it with other information that you’ve provided to them or
+                    thatthey’ve collected from your use of their services.
                 </Typography>
                 <Typography variant="body1" gutterBottom>
                     Please select the types of cookies you would like to allow:
                 </Typography>
                 <Box>
-                    <Button variant={allowFunctionalCookies ? 'contained' : 'outlined'}
-                            onClick={handleToggleFunctionalCookies} className={classes.button}>
+                    <Button
+                        variant={allowFunctionalCookies ? 'contained' : 'outlined'}
+                        onClick={handleToggleFunctionalCookies}
+                        className={classes.button}
+                    >
                         Functional Cookies
                     </Button>
-                    <Button variant={allowAnalyticsCookies ? 'contained' : 'outlined'}
-                            onClick={handleToggleAnalyticsCookies} className={classes.button}>
+                    <Button
+                        variant={allowAnalyticsCookies ? 'contained' : 'outlined'}
+                        onClick={handleToggleAnalyticsCookies}
+                        className={classes.button}
+                    >
                         Analytics Cookies
                     </Button>
-                    <Button variant={allowMarketingCookies ? 'contained' : 'outlined'}
-                            onClick={handleToggleMarketingCookies} className={classes.button}>
+                    <Button
+                        variant={allowMarketingCookies ? 'contained' : 'outlined'}
+                        onClick={handleToggleMarketingCookies}
+                        className={classes.button}
+                    >
                         Marketing Cookies
                     </Button>
                 </Box>
