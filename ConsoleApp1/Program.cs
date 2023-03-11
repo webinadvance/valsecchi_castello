67713462fb24c89849367042e48566830d17a301
webinadvance <webinadvance@gmail.com>
2023-03-11 14:34:28 +0100

    gfdgfd

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,11 +1,67 @@
 ﻿using System.Drawing;
 using System.Drawing.Imaging;
+using System.Text;
+using System.Text.Json;
+using Encoder = System.Drawing.Imaging.Encoder;
 
 namespace ImageConverter;
 
 internal class Program
 {
     private static void Main(string[] args)
+    {
+        CreateLowRes();
+        CreateJsonGallery();
+    }
+
+    private static void CreateJsonGallery()
+    {
+        var rootPath =
+            @"C:\Users\webin\RiderProjects\valsecchi_castello\ASP.NETCoreWebApplication5\ClientApp\public\assets";
+        var directories = new List<object>();
+
+        foreach (var subdirectory in Directory.GetDirectories(rootPath))
+        {
+            var directoryData = new List<object>();
+            var directoryName = new DirectoryInfo(subdirectory).Name;
+
+            foreach (var file in Directory.GetFiles(subdirectory, "*.jpg")
+                         .Where(path => !path.EndsWith("_low.jpg")))
+            {
+                var fileName = Path.GetFileName(file);
+
+                directoryData.Add(new
+                {
+                    src = $"/{directoryName}/{fileName}",
+                    title = fileName
+                });
+            }
+
+            directories.Add(new
+            {
+                title = directoryName,
+                data = directoryData
+            });
+        }
+
+        var options = new JsonSerializerOptions
+        {
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+        };
+
+        var json = JsonSerializer.Serialize(directories, options);
+        var byteArray = Encoding.ASCII.GetBytes(json);
+        var stream = new MemoryStream(byteArray);
+
+        var filePath =
+            Path.Combine(
+                @"C:\Users\webin\RiderProjects\valsecchi_castello\ASP.NETCoreWebApplication5\ClientApp\public\data",
+                "gallery.json");
+        System.IO.File.WriteAllText(filePath, json);
+    }
+
+
+    private static void CreateLowRes()
     {
         // Specify the path to the directory containing the images
         var path =
@@ -27,9 +83,9 @@ internal class Program
                 // Calculate the new height and width while maintaining the aspect ratio
                 var originalWidth = originalImage.Width;
                 var originalHeight = originalImage.Height;
-                var ratio = originalWidth / (double) originalHeight;
+                var ratio = originalWidth / (double)originalHeight;
                 var newWidth = Math.Min(originalWidth, 500);
-                var newHeight = (int) Math.Round(newWidth / ratio);
+                var newHeight = (int)Math.Round(newWidth / ratio);
 
                 // Create a new Bitmap with the new size
                 using (var resizedImage = new Bitmap(originalImage, newWidth, newHeight))
