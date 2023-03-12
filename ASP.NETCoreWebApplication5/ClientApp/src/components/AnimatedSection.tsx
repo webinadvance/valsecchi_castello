bb3e228164d31991a54f298ba49fa8a6fa4ebd9f
webinadvance <webinadvance@gmail.com>
2023-03-12 21:55:26 +0100

    dsadsa

--- ASP.NETCoreWebApplication5/ClientApp/src/components/AnimatedSection.tsx
+++ ASP.NETCoreWebApplication5/ClientApp/src/components/AnimatedSection.tsx
@@ -1,42 +1,42 @@
-import { animated, useInView } from '@react-spring/web'
-import { type ReactNode, useEffect } from 'react'
-import { useMediaQuery } from '@mui/material'
+import {animated, useInView} from "@react-spring/web";
+import {type ReactNode, useEffect} from "react";
+import {useMediaQuery} from "@mui/material";
 
 export interface AnimatedSectionProps {
-  children: ReactNode
+    children: ReactNode;
 }
 
-export const AnimatedSection = ({ children }: AnimatedSectionProps) => {
-  const [ref, inView] = useInView(
-    () => ({
-      from: {
-        opacity: 0,
-        y: 100
-			},
-      to: {
-        opacity: 1,
-        y: 0
-			},
-      once: true,
-      threshold: 0.2
-		})
-  )
+export const AnimatedSection = ({children}: AnimatedSectionProps) => {
+    const [ref, inView] = useInView(
+        () => ({
+            from: {
+                opacity: 0,
+                y: 100
+            },
+            to: {
+                opacity: 1,
+                y: 0
+            },
+            once: true,
+            threshold: 0.2
+        })
+    );
 
-  const isMobile = useMediaQuery('(max-width:1024px)')
+    const isMobile = useMediaQuery("(max-width:1024px)");
 
-	useEffect(() => {
-    if (inView) {
-      console.log('In view!')
-		}
-  }, [inView])
+    useEffect(() => {
+        if (inView) {
+            console.log("In view!");
+        }
+    }, [inView]);
 
-	if (isMobile) {
-    return <>{children}</>
-	}
+    if (isMobile) {
+        return <section>{children}</section>;
+    }
 
-  return (
-		<animated.section ref={ref} style={inView}>
-			{children}
-		</animated.section>
-  );
-}
+    return (
+        <animated.section ref={ref} style={inView}>
+            {children}
+        </animated.section>
+    );
+};
--- ASP.NETCoreWebApplication5/ClientApp/src/components/PhotoGallery.tsx
+++ ASP.NETCoreWebApplication5/ClientApp/src/components/PhotoGallery.tsx
@@ -1,79 +1,79 @@
-import * as React from 'react'
-import {styled} from '@mui/material/styles'
-import Box from '@mui/material/Box'
-import ImageList from '@mui/material/ImageList'
-import ImageListItem from '@mui/material/ImageListItem'
-import Dialog from '@mui/material/Dialog'
-import useMediaQuery from '@mui/material/useMediaQuery'
-import IconButton from '@mui/material/IconButton'
-import CloseIcon from '@mui/icons-material/Close'
+import * as React from "react";
+import {styled} from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import ImageList from "@mui/material/ImageList";
+import ImageListItem from "@mui/material/ImageListItem";
+import Dialog from "@mui/material/Dialog";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import IconButton from "@mui/material/IconButton";
+import CloseIcon from "@mui/icons-material/Close";
 import {useTranslation} from "react-i18next";
-import {LazyLoadImage} from 'react-lazy-load-image-component';
+import {LazyLoadImage} from "react-lazy-load-image-component";
 
 const PreviewContainer = styled(Box)({
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    height: '100vh',
-    '& img': {
-        objectFit: 'contain',
-        maxHeight: '100%',
-        maxWidth: '100%'
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    height: "100vh",
+    "& img": {
+        objectFit: "contain",
+        maxHeight: "100%",
+        maxWidth: "100%"
     },
-})
+});
 
 const CloseButton = styled(IconButton)({
-    position: 'absolute',
+    position: "absolute",
     top: 0,
     right: 0
-})
+});
 
 const Overline = styled(Box)({
-    position: 'absolute',
+    position: "absolute",
     top: 0,
     left: 0,
-    width: '100%',
-    height: '100%',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    color: '#fff',
-    backgroundColor: 'rgba(0,0,0,0.5)',
+    width: "100%",
+    height: "100%",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    color: "#fff",
+    backgroundColor: "rgba(0,0,0,0.5)",
     opacity: 0,
-    transition: 'opacity 0.2s ease-in-out',
-    '&:hover': {
+    transition: "opacity 0.2s ease-in-out",
+    "&:hover": {
         opacity: 1
     },
-    '& span': {
-        fontSize: '1.2rem',
-        fontWeight: 'bold',
-        textTransform: 'uppercase',
-        letterSpacing: '0.1rem',
-        textAlign: 'center'
+    "& span": {
+        fontSize: "1.2rem",
+        fontWeight: "bold",
+        textTransform: "uppercase",
+        letterSpacing: "0.1rem",
+        textAlign: "center"
     },
-})
+});
 
 interface Props {
-    images: any[]
+    images: any[];
 }
 
 const PhotoGallery = (props: Props) => {
-    const [open, setOpen] = React.useState(false)
-    const [selectedImage, setSelectedImage] = React.useState('')
-    const isMobile = useMediaQuery('(max-width:1024px)')
-    const {t} = useTranslation()
+    const [open, setOpen] = React.useState(false);
+    const [selectedImage, setSelectedImage] = React.useState("");
+    const isMobile = useMediaQuery("(max-width:1024px)");
+    const {t} = useTranslation();
     const handleClickOpen = (image: string) => {
-        setSelectedImage(image)
-        setOpen(true)
+        setSelectedImage(image);
+        setOpen(true);
     };
 
     const handleClose = () => {
-        setSelectedImage('')
-        setOpen(false)
+        setSelectedImage("");
+        setOpen(false);
     };
 
     return (
-        <Box sx={{maxWidth: isMobile ? "99vw" : "60vw", margin: "0 auto"}}>
+        <Box sx={{maxWidth: isMobile ? "90vw" : "60vw", margin: "0 auto"}}>
             <ImageList sx={{
                 overflow: "hidden"
             }} variant="quilted" cols={isMobile ? 1 : 3} gap={8}>
@@ -81,31 +81,31 @@ const PhotoGallery = (props: Props) => {
                     <ImageListItem key={image.src}>
                         <Box
                             sx={{
-                                width: '100%',
+                                width: "100%",
                                 height: 0,
-                                paddingBottom: '70%',
-                                position: 'relative',
-                                cursor: 'pointer',
+                                paddingBottom: "70%",
+                                position: "relative",
+                                cursor: "pointer",
                                 overflow: "none"
                             }}
                             onClick={() => {
-                                handleClickOpen(image.src)
+                                handleClickOpen(image.src);
                             }}
                         >
                             <LazyLoadImage
-                                src={"/assets" + image.src.replace('.jpg', '_low.jpg') + `?${Math.random()}`}
+                                src={"/assets" + image.src.replace(".jpg", "_low.jpg") + `?${Math.random()}`}
                                 height={"100%"}
                                 style={{
-                                    position: 'absolute',
+                                    position: "absolute",
                                     top: 0,
                                     left: 0,
-                                    width: '100%',
-                                    height: '100%',
-                                    objectFit: 'cover'
+                                    width: "100%",
+                                    height: "100%",
+                                    objectFit: "cover"
                                 }}
                             />
                             <Overline>
-                                <span>{t('t_image_fullsize')}</span>
+                                <span>{t("t_image_fullsize")}</span>
                             </Overline>
                         </Box>
                     </ImageListItem>
@@ -125,6 +125,6 @@ const PhotoGallery = (props: Props) => {
             </Dialog>
         </Box>
     );
-}
+};
 
-export default PhotoGallery
+export default PhotoGallery;
