1a25a01847623048608f0710fda741abd7401473
webinadvance <webinadvance@gmail.com>
2023-03-13 12:31:16 +0100

    dsad

--- ASP.NETCoreWebApplication5/ClientApp/src/components/AdminGallery.tsx
+++ ASP.NETCoreWebApplication5/ClientApp/src/components/AdminGallery.tsx
@@ -1,16 +1,31 @@
 import React, {useEffect, useState} from "react";
 import axios from "axios";
-import {useMediaQuery, useTheme,} from "@mui/material";
+import {useMediaQuery, useTheme} from "@mui/material";
 import {Api2} from "../Api2";
 import {AdminGalleryRender} from "./AdminGalleryRender";
 
 const AdminGallery = () => {
     const [images, setImages] = useState<any>();
-
     const [newImageName, setNewImageName] = useState("");
     const [selectedFile, setSelectedFile] = useState<any>();
-
     const [loading, setLoading] = useState(false);
+    const [imageToDelete, setImageToDelete] = useState(null);
+    const [newTitle, setNewTitle] = useState<Record<string, any>>({});
+
+    const theme = useTheme();
+    const isMobile = useMediaQuery(theme.breakpoints.down("md"));
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get(`./data/gallery.json?${Date.now()}`);
+                setImages(response.data);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+        fetchData();
+    }, []);
 
     const handleFileChange = (e: any) => {
         setSelectedFile(e.target.files[0]);
@@ -28,25 +43,6 @@ const AdminGallery = () => {
         setLoading(false);
     };
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await axios.get(`./data/gallery.json?${Date.now()}`);
-                setImages(response.data);
-            } catch (error) {
-                console.error(error);
-            }
-        };
-        fetchData();
-    }, []);
-
-    const [imageToDelete, setImageToDelete] = useState(null);
-
-    const theme = useTheme();
-    const isMobile = useMediaQuery(theme.breakpoints.down("md"));
-
-    let newTitle: Record<string, any> = {};
-
     async function deleteImage() {
         setLoading(true);
         await Api2.deleteImage(imageToDelete ?? "");
@@ -60,7 +56,7 @@ const AdminGallery = () => {
             await axios.post("/api/db/changedirname", null, {
                 params: {
                     oldValue: rootTitle.title,
-                    newValue: newTitle[rootTitle.title]
+                    newValue: newTitle[rootTitle.title],
                 },
                 withCredentials: true,
             });
@@ -69,7 +65,18 @@ const AdminGallery = () => {
         }
     }
 
-    return AdminGalleryRender(loading, imageToDelete, setImageToDelete, deleteImage, isMobile, images, newTitle, saveTitle, handleFileChange, handleFileUpload);
+    return AdminGalleryRender(
+        loading,
+        imageToDelete,
+        setImageToDelete,
+        deleteImage,
+        isMobile,
+        images,
+        newTitle,
+        saveTitle,
+        handleFileChange,
+        handleFileUpload
+    );
 };
 
 export default AdminGallery;
